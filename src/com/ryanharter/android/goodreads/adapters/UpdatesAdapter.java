8c622c5967093008ecd5430be4c9467138a100bc
Ryan Harter <rharter@Ryans-Elementary-PC.(none)>
2012-08-28 08:46:01 -0500

    Added imageloader library for update images.

--- src/com/ryanharter/android/goodreads/adapters/UpdatesAdapter.java
+++ src/com/ryanharter/android/goodreads/adapters/UpdatesAdapter.java
@@ -1,56 +1,74 @@
-// package com.ryanharter.android.goodreads.adapters;
-// 
-// import com.goodreads.api.v1.Update;
-// import com.ryanharter.android.goodreads.R;
-// 
-// import android.content.Context;
-// import android.view.LayoutInflater;
-// import android.view.View;
-// import android.view.ViewGroup;
-// import android.widget.ArrayAdapter;
-// import android.widget.ImageView;
-// import android.widget.TextView;
-// 
-// public class UpdatesAdapter extends ArrayAdapter<Update> {
-// 	
-// 	private Context context;
-// 	
-// 	/**
-// 	 * Replaces the normal constructors and builds a new 
-// 	 * NavigationAdapter.
-// 	 */
-// 	public UpdatesAdapter(Context context) {	
-// 		super(context, 0);
-// 		this.context = context;
-// 	}
-// 	
-// 	/**
-// 	 * Creates
-// 	 */
-// 	@Override
-// 	public View getView (int position, View convertView, ViewGroup parent) {
-// 		View v = convertView;
-// 		NavigationView navView = null;
-// 		
-// 		if (v == null) {
-// 			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-// 			v = inflater.inflate(R.layout.row_navigation, parent, false);
-// 			
-// 			// Set up the holder
-// 			navView = new NavigationView();
-// 			navView.image = (ImageView) v.findViewById(R.id.image);
-// 			navView.name = (TextView) v.findViewById(R.id.name);
-// 			
-// 			// Store the view holder
-// 			v.setTag(navView);
-// 		} else {
-// 			navView = (NavigationView) v.getTag();
-// 		}
-// 		
-// 		NavigationItem item = getItem(position);
-// 		navView.image.setImageResource(item.imageId);
-// 		navView.name.setText(item.name);
-// 		
-// 		return v;
-// 	}
-// }
+package com.ryanharter.android.goodreads.adapters;
+
+import java.util.List;
+
+import com.goodreads.api.v1.Update;
+
+import com.ryanharter.android.goodreads.R;
+
+import com.google.android.imageloader.ImageLoader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.util.Log;
+
+public class UpdatesAdapter extends ArrayAdapter<Update> {
+	private static final String TAG = "UpdatesAdapter";
+	
+ 	private Context mContext;
+	
+	/**
+	 * Replaces the normal constructors and builds a new 
+	 * NavigationAdapter.
+ 	 */
+ 	public UpdatesAdapter(Context context, List<Update> updates) {	
+ 		super(context, 0);
+ 		mContext = context;
+ 		addAll(updates);
+ 	}
+ 	
+ 	/**
+ 	 * Creates a view to display a Goodreads Update.
+ 	 */
+ 	@Override
+ 	public View getView (int position, View convertView, ViewGroup parent) {
+ 		View v = convertView;
+ 		UpdateViewHolder holder = null;
+ 		
+ 		Log.d(TAG, "Getting view for position: " + position);
+ 		
+ 		if (v == null) {
+ 			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+ 			v = inflater.inflate(R.layout.row_update, parent, false);
+ 			
+ 			// Set up the holder
+ 			holder = new UpdateViewHolder();
+ 			holder.image = (ImageView) v.findViewById(R.id.image);
+ 			holder.action = (TextView) v.findViewById(R.id.action);
+ 			
+ 			// Store the view holder
+ 			v.setTag(holder);
+ 		} else {
+ 			holder = (UpdateViewHolder) v.getTag();
+ 		}
+ 		
+ 		Update update = getItem(position);
+ 		holder.action.setText(update.getActionText());
+ 		
+ 		ImageLoader imageLoader = new ImageLoader();
+ 		Log.d(TAG, "Loading image for url: " + update.getImageUrl());
+ 		imageLoader.bind(this, holder.image, update.getImageUrl());
+ 		
+ 		return v;
+ 	}
+ 	
+ 	private static class UpdateViewHolder {
+ 		public ImageView image;
+ 		public TextView action;
+ 	}
+ }
--- src/com/ryanharter/android/goodreads/ui/UpdatesFragment.java
+++ src/com/ryanharter/android/goodreads/ui/UpdatesFragment.java
@@ -1,8 +1,13 @@
 package com.ryanharter.android.goodreads.ui;
 
-import com.goodreads.api.v1.Update;
-import com.ryanharter.android.goodreads.R;
+import java.util.List;
 
+import com.goodreads.api.v1.Update;
+import com.goodreads.api.v1.GoodreadsService;
+import com.ryanharter.android.goodreads.R;
+import com.ryanharter.android.goodreads.adapters.UpdatesAdapter;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -10,21 +15,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.util.Log;
 
 public class UpdatesFragment extends ListFragment {
+	private static final String TAG = "UpdatesFragment";
+	
+	private List<Update> mUpdates;
+	private UpdatesAdapter mAdapter;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_updates, container, false);
 		
-		ListView listview = (ListView) view.findViewById(android.R.id.list);
-		
-		String[] updates = { "Update 1", "Update 2", "Update 3", "Update 4" };
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1, updates);
-		
-		setListAdapter(adapter);
+		new GetUpdatesTask().execute();
 		
 		return view;
+	}
+	
+	public class GetUpdatesTask extends AsyncTask<Void, Void, List<Update>> {
+		
+		protected List<Update> doInBackground(Void... nothing) {
+			List<Update> updates = null;
+			try {
+				updates = GoodreadsService.getFriendsUpdates();
+			} catch (Exception e) {
+				Log.e(TAG, "Failed to get friends updates.", e);
+			}
+			return updates;
+		}
+		
+		protected void onPostExecute(List<Update> updates) {
+			mUpdates = updates;
+			
+			mAdapter = new UpdatesAdapter(getActivity(), mUpdates);
+			setListAdapter(mAdapter);
+		}
 	}
 }
